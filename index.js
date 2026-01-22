e23c69d56b107c432b6ee12925a9382c9470553a
tonarinonikibi <242139187+tonarinonikibi@users.noreply.github.com>
2026-01-22 13:31:32 +0900

    Update index.js
    Gemini

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict' ;
-const title = document.getElementById('h1');
+const title = document.querySelector('h1');
 let angle = 0;
 
 function shake() {
+  if (!title) return;
+  
   const x = Math.sin(angle) * 6;
   title.style.transform = `translateX(${x}px)`;
   angle += 0.08;
