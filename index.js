ef80dc9d7310fa770fe06aa0d36c0fd0bd9cde7f
tonarinonikibi <242139187+tonarinonikibi@users.noreply.github.com>
2026-01-22 12:02:09 +0900

    Create index.js
    jsファイルの追加 横揺れ

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict' ;
+const title = document.querySelector("h1");
+let angle = 0;
+
+function shake() {
+  const x = Math.sin(angle) * 6;
+  title.style.transform = `translateX(${x}px)`;
+  angle += 0.08;
+  requestAnimationFrame(shake);
+}
+
+shake();
