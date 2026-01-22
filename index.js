4d6374954de27d175e7003ae43b5e0fd2190bb48
tonarinonikibi <242139187+tonarinonikibi@users.noreply.github.com>
2026-01-22 12:27:15 +0900

    Update index.js
    h1だけ

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 'use strict' ;
-const targets = document.getElementById('h1, img');
-
+const title = document.getElementById('h1');
 let angle = 0;
 
 function shake() {
   const x = Math.sin(angle) * 6;
-
-  targets.forEach((el) => {
-    el.style.transform = `translateX(${x}px)`;
-  });
-
+  title.style.transform = `translateX(${x}px)`;
   angle += 0.08;
   requestAnimationFrame(shake);
 }
