8578f065e3a115eea4366550397238cb2aa261fd
tonarinonikibi <242139187+tonarinonikibi@users.noreply.github.com>
2026-01-22 12:52:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let angle = 0;
 
 function shake() {
   const x = Math.sin(angle) * 6;
-  title.style.transform = "translateX(" + x + "px)";
+  title.style.transform = `translateX(${x}px)`
   angle += 0.08;
   requestAnimationFrame(shake);
 }
