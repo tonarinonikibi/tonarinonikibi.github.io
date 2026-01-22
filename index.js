89fccd1b61f24715ac22716378a0d0185497fbd7
tonarinonikibi <242139187+tonarinonikibi@users.noreply.github.com>
2026-01-22 13:20:36 +0900

    Update index.js
    Commit adb6606戻す

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict' ;
 const title = document.getElementById('h1');
 let angle = 0;
 
 function shake() {
   const x = Math.sin(angle) * 6;
-  title.style.transform = `translateX(${x}px)`
+  title.style.transform = `translateX(${x}px)`;
   angle += 0.08;
   requestAnimationFrame(shake);
 }
